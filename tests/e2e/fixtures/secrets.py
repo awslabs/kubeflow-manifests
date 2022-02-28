572fc64caf2054e90ec6d227e1b548683083feef
rrrkharse <rkharse@amazon.com>
2022-02-28 09:41:35 -0800

    Decouple RDS and S3 KFP installation

--- tests/e2e/fixtures/secrets.py
+++ tests/e2e/fixtures/secrets.py
@@ -4,7 +4,7 @@ import pytest
 
 from e2e.utils.utils import kubectl_apply
 
-from e2e.fixtures.cluster import associate_iam_oidc_provider, create_iam_service_account
+from e2e.fixtures.cluster import associate_iam_oidc_provider, create_iam_service_account, delete_iam_service_account
 from e2e.utils.k8s_core_api import create_namespace
 
 from e2e.utils.constants import (
@@ -31,13 +31,15 @@ def aws_secrets_driver(cluster, region):
 
     iam_policies = [IAM_AWS_SSM_READ_ONLY_POLICY, IAM_SECRETS_MANAGER_READ_WRITE_POLICY]
 
+    delete_iam_service_account(
+        KUBEFLOW_SERVICE_ACCOUNT_NAME, KUBEFLOW_NAMESPACE, cluster, region
+    )
     create_iam_service_account(
         KUBEFLOW_SERVICE_ACCOUNT_NAME, KUBEFLOW_NAMESPACE, cluster, region, iam_policies
     )
 
     secrets_store_csi_driver.install()
     secrets_store_csi_driver_provider_aws.install()
-
 
 def create_secret_string(secrets_dict):
     return json.dumps(secrets_dict)
