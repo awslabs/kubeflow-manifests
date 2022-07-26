31e062a06658a6db4e422c9d47b5c6d363336076
jsitu <jsitu@88665a43d1bb.ant.amazon.com>
2022-07-26 15:35:29 -0700

    wrap original kustomize fixture into a new fixture 'installation', it includes setup and tear down method for both kustomize and helm

--- tests/e2e/fixtures/installation.py
+++ tests/e2e/fixtures/installation.py
@@ -1,5 +1,5 @@
 """
-Kustomize fixture module
+Kubeflow Installation fixture module
 """
 
 import subprocess
@@ -27,6 +27,17 @@ def apply_kustomize(path):
         apply_retcode = subprocess.call(f"kubectl apply -f {tmp.name}".split())
         assert apply_retcode == 0
 
+def install_helm(chart_name, path):
+    """
+    Equivalent to:
+
+    helm install <chart_name> <path>
+    
+    """
+
+    with tempfile.NamedTemporaryFile() as tmp:
+        install_retcode = subprocess.call(f"helm install {chart_name} {path}".split())
+        assert install_retcode == 0
 
 def delete_kustomize(path):
     """
@@ -39,7 +50,20 @@ def delete_kustomize(path):
     with tempfile.NamedTemporaryFile() as tmp:
         build_retcode = subprocess.call(f"kustomize build {path} -o {tmp.name}".split())
         assert build_retcode == 0
+    
+        delete_retcode = subprocess.call(f"kubectl delete -f {tmp.name}".split())
+        assert delete_retcode == 0
 
+def uninstall_helm(chart_name):
+    """
+    Equivalent to:
+
+    helm uninstall <chart_name>
+
+    """
+   
+    uninstall_retcode = subprocess.call(f"helm uninstall {chart_name}".split())
+    assert uninstall_retcode == 0
 
 @pytest.fixture(scope="class")
 def configure_manifests():
@@ -59,8 +83,8 @@ def clone_upstream():
 
 
 @pytest.fixture(scope="class")
-def kustomize(
-    metadata, cluster, clone_upstream, configure_manifests, kustomize_path, request
+def installation(
+    metadata, cluster, clone_upstream, configure_manifests, installation_path, installation_option, request
 ):
     """
     This fixture is created once for each test class.
@@ -73,21 +97,54 @@ def kustomize(
     """
 
     def on_create():
-        wait_for(lambda: apply_kustomize(kustomize_path), timeout=20 * 60)
-        time.sleep(5 * 60)  # wait a bit for all pods to be running
+
+        if (installation_option == 'kustomize'):
+            wait_for(lambda: apply_kustomize(installation_path), timeout=20 * 60)
+            time.sleep(5 * 60)  # wait a bit for all pods to be running
         # TODO: verify this programmatically
+        if (installation_option == 'helm'):
+            preq_helm_charts = installation_path[0]
+            comp_helm_charts = installation_path[1]
+
+            for helm_chart in preq_helm_charts:
+                chart_path = helm_chart[0]
+                chart_name = helm_chart[1]
+                wait_for(lambda: install_helm(chart_name, chart_path), timeout=3 * 60)
+                
+            
+            for helm_chart in comp_helm_charts:
+                chart_path = helm_chart[0]
+                chart_name = helm_chart[1]
+                wait_for(lambda: install_helm(chart_name, chart_path), timeout=3 * 60)
+
+        time.sleep(60)
+        # wait a bit for all pods to be running
 
     def on_delete():
-        # deleting the kubeflow deployment deletes the load balancer controller at the same time as ingress
-        # the problem with this is the ingress managed load balacer does not get cleaned up as there is no controller 
+         # deleting the kubeflow deployment deletes the load balancer controller at the same time as ingress
+            # the problem with this is the ingress managed load balacer does not get cleaned up as there is no controller 
         subprocess.call(f"kubectl delete ingress -n istio-system --all".split())
-        # load balancer controller does not place a finalizer on the ingress and so deleting the attached load balancer is asynhronous
-        # adding a random wait to allow controller to delete the load balancer
-        # TODO: implement a better check
+        subprocess.call(f"kubectl delete profile --all".split())
+            # load balancer controller does not place a finalizer on the ingress and so deleting the attached load balancer is asynhronous
+            # adding a random wait to allow controller to delete the load balancer
+            # TODO: implement a better check
         time.sleep(2 * 60)
-
-        delete_kustomize(kustomize_path)
+        if (installation_option == 'kustomize'):
+            delete_kustomize(installation_path)
+        
+        if (installation_option == 'helm'):
+            preq_helm_charts = installation_path[0]
+            comp_helm_charts = installation_path[1]
+            preq_helm_charts.reverse()
+            comp_helm_charts.reverse()
+            for helm_chart in preq_helm_charts:
+                chart_name = helm_chart[1]
+                uninstall_helm(chart_name)
+            
+            for helm_chart in comp_helm_charts:
+                chart_name = helm_chart[1]
+                uninstall_helm(chart_name)
 
     configure_resource_fixture(
-        metadata, request, kustomize_path, "kustomize_path", on_create, on_delete
+        metadata, request, installation_path, "installation_path", on_create, on_delete
     )
