54e00f71d46219b75fd1e7d95df81144bd81abc5
Suraj Kota <surakota@amazon.com>
2022-09-13 07:35:56 +0000

    Add cognito terraform module and a sample test action

---
+++ iaac/terraform/aws-infra/cognito/variables.tf
@@ -0,0 +1,9 @@
+variable "cognito_user_pool_name" {
+  description = "Cognito UserPool name"
+  type        = string
+}
+
+variable "aws_route53_zone_id" {
+  description = "Route 53 hosted zone"
+  type        = string
+}
