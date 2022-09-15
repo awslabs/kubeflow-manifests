f2e84429eca9a0c5b0be17ba3618c90095dc6c58
Suraj Kota <surakota@amazon.com>
2022-09-15 12:15:28 -0700

    add subdomain and cognito ingress terraform module (#366)
    **Description of your changes:**
    - Module to create subdomain and add test in gh-action
    - Module to create ingress. Did not add test since it needs a cluster
    but have tested it locally. Will add an end to end test with the
    complete deployment after #358 is merged
    
    **Testing:**
    - [x] e2e tests - create and destroy in gh-action
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

---
+++ iaac/terraform/aws-infra/subdomain/certificate.tf
@@ -0,0 +1,30 @@
+resource "aws_acm_certificate" "root_domain_deployment_region" {
+  domain_name       = "*.${data.aws_route53_zone.root.name}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "certificate_validation_root_domain" {
+  for_each = {
+    for dvo in aws_acm_certificate.root_domain_deployment_region.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.root.zone_id
+}
+
+resource "aws_acm_certificate_validation" "root_domain_deployment_region" {
+  certificate_arn         = aws_acm_certificate.root_domain_deployment_region.arn
+  validation_record_fqdns = [for record in aws_route53_record.certificate_validation_root_domain : record.fqdn]
+}
---
+++ iaac/terraform/aws-infra/subdomain/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_route53_root_zone_name" {
+  description = "TOP LEVEL/ROOT Route 53 hosted zone name (e.g. example.com). Must match exactly one zone."
+  type        = string
+}
+
+variable "aws_route53_subdomain_zone_name" {
+  description = "SUBDOMAIN Route 53 hosted zone name(e.g. platform.example.com) which will be created for Kubeflow Platform"
+  type        = string
+}
