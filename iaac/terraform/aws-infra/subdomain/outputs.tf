80dda285dc3ed80a696c9586110b6b567f124c0e
Suraj Kota <surakota@amazon.com>
2022-09-15 19:13:13 +0000

    update outoput name

--- iaac/terraform/aws-infra/subdomain/outputs.tf
+++ iaac/terraform/aws-infra/subdomain/outputs.tf
@@ -1,4 +1,4 @@
-output "subdomain_zone_name" {
+output "aws_route53_subdomain_zone_name" {
   description = "Subdomain which will be used for Kubeflow Platform (e.g. platform.example.com)"
   value       = aws_route53_zone.platform.name
 }
