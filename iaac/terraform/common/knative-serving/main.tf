6ae867b30cde876998267242249b5eee2d7078c6
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-12-14 16:46:39 -0800

    fix left over merge

--- iaac/terraform/common/knative-serving/main.tf
+++ iaac/terraform/common/knative-serving/main.tf
@@ -1,11 +1,5 @@
 module "helm_addon" {
-<<<<<<< HEAD
   source        = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.18.1"
   helm_config   = local.helm_config
   addon_context = var.addon_context
-=======
-  source            = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons/helm-addon?ref=v4.12.1"
-  helm_config       = local.helm_config
-  addon_context     = var.addon_context
->>>>>>> main
 }
