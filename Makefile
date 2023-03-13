9fcd434257fef6d5bfa1667647cd67fae53cb540
ryansteakley <37981995+ryansteakley@users.noreply.github.com>
2023-03-13 09:34:47 -0700

    kustomize manifests for irsa (#571)
    **Description of your changes:**
    Moves current s3 kustomize files into s3-static folder, andmakes changes
    necessary for irsa in s3.
    
    Installation script changes coming in separate pr
    
    **Testing:**
    - [ ] Unit tests pass
    - [ ] e2e tests pass
    - Details about new tests (If this PR adds a new feature)
    - Details about any manual tests performed
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- Makefile
+++ Makefile
@@ -93,15 +93,29 @@ bootstrap-ack: verify-cluster-variables connect-to-eks-cluster
 	yq e '.cluster.region=env(CLUSTER_REGION)' -i tests/e2e/utils/ack_sm_controller_bootstrap/config.yaml
 	cd tests/e2e && PYTHONPATH=.. python3.8 utils/ack_sm_controller_bootstrap/setup_sm_controller_req.py
 
+bootstrap-pipelines: verify-cluster-variables connect-to-eks-cluster
+	yq e '.cluster.name=env(CLUSTER_NAME)' -i tests/e2e/utils/pipelines/config.yaml
+	yq e '.cluster.region=env(CLUSTER_REGION)' -i tests/e2e/utils/pipelines/config.yaml
+	cd tests/e2e && PYTHONPATH=.. python3.8 utils/pipelines/setup_pipelines_irsa.py
+
 cleanup-ack-req: verify-cluster-variables
 	yq e '.cluster.name=env(CLUSTER_NAME)' -i tests/e2e/utils/ack_sm_controller_bootstrap/config.yaml
 	yq e '.cluster.region=env(CLUSTER_REGION)' -i tests/e2e/utils/ack_sm_controller_bootstrap/config.yaml
 	cd tests/e2e && PYTHONPATH=.. python3.8 utils/ack_sm_controller_bootstrap/cleanup_sm_controller_req.py
 
+cleanup-pipelines-req: verify-cluster-variables
+	yq e '.cluster.name=env(CLUSTER_NAME)' -i tests/e2e/utils/pipelines/config.yaml
+	yq e '.cluster.region=env(CLUSTER_REGION)' -i tests/e2e/utils/pipelines/config.yaml
+	cd tests/e2e && PYTHONPATH=.. python3.8 utils/pipelines/cleanup_pipelines_irsa.py
+
 deploy-kubeflow: bootstrap-ack
 	$(eval DEPLOYMENT_OPTION:=vanilla)
 	$(eval INSTALLATION_OPTION:=kustomize)
-	cd tests/e2e && PYTHONPATH=.. python3.8 utils/kubeflow_installation.py --deployment_option $(DEPLOYMENT_OPTION) --installation_option $(INSTALLATION_OPTION) --cluster_name $(CLUSTER_NAME)
+	$(eval CREDENTIAL_OPTION:=irsa)
+	if [ "$(CREDENTIAL_OPTION)" = "irsa" ]; then \
+		make bootstrap-pipelines; \
+	fi
+	cd tests/e2e && PYTHONPATH=.. python3.8 utils/kubeflow_installation.py --deployment_option $(DEPLOYMENT_OPTION) --installation_option $(INSTALLATION_OPTION) --credential_option $(CREDENTIAL_OPTION) --cluster_name $(CLUSTER_NAME)
 
 delete-kubeflow:
 	$(eval DEPLOYMENT_OPTION:=vanilla)
---
+++ tests/e2e/utils/pipelines/setup_pipelines_irsa.py
@@ -0,0 +1,112 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import json
+import boto3
+
+from e2e.utils.utils import (
+    load_json_file,
+    get_iam_client,
+    get_eks_client,
+)
+from e2e.fixtures.cluster import (
+    associate_iam_oidc_provider,
+)
+from e2e.utils.aws.iam import IAMPolicy
+from e2e.utils.pipelines import common
+from e2e.utils.config import configure_env_file
+from e2e.utils.utils import print_banner, load_yaml_file, write_env_to_yaml, exec_shell
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def profile_trust_policy(cluster, region, account_id):
+    eks_client = get_eks_client(region=region)
+
+    resp = eks_client.describe_cluster(name=cluster)
+    oidc_url = resp["cluster"]["identity"]["oidc"]["issuer"].split("https://")[1]
+
+    trust_policy = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                    "Federated": f"arn:aws:iam::{account_id}:oidc-provider/{oidc_url}"
+                },
+                "Action": "sts:AssumeRoleWithWebIdentity",
+                "Condition": {
+                    "StringEquals": {
+                        f"{oidc_url}:aud": "sts.amazonaws.com",
+                        f"{oidc_url}:sub": [
+                            "system:serviceaccount:kubeflow:ml-pipeline",
+                        ],
+                    }
+                },
+            }
+        ],
+    }
+    return json.dumps(trust_policy)
+
+
+def create_pipeline_oidc_role(cluster_name, region):
+    iam_client = get_iam_client(region=region)
+    acc_id = get_account_id()
+    role_name = f"{common.PIPELINE_OIDC_ROLE_NAME_PREFIX}-{cluster_name}"
+
+    resp = iam_client.create_role(
+        RoleName=role_name,
+        AssumeRolePolicyDocument=profile_trust_policy(cluster_name, region, acc_id),
+    )
+    oidc_role_arn = resp["Role"]["Arn"]
+
+    print(f"Created IAM Role : {oidc_role_arn}")
+
+    iam_client.attach_role_policy(
+        RoleName=role_name, PolicyArn="arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    )
+
+
+def get_role_arn(role_name, region):
+    iam_client = get_iam_client(region=region)
+    resp = iam_client.get_role(RoleName=role_name)
+    oidc_role_arn = resp["Role"]["Arn"]
+    return oidc_role_arn
+
+
+def get_account_id():
+    return boto3.client("sts").get_caller_identity().get("Account")
+
+
+def write_params(oidc_role_arn, region, env_file_path, config_file_path):
+    write_env_to_yaml({"pipeline_oidc_role": oidc_role_arn}, config_file_path)
+    print(f"Config file written to : {config_file_path}")
+
+
+if __name__ == "__main__":
+    print_banner("Reading Config")
+    config_file_path = common.CONFIG_FILE_PATH
+    cfg = load_yaml_file(file_path=config_file_path)
+    cluster_region = cfg["cluster"]["region"]
+    cluster_name = cfg["cluster"]["name"]
+
+    print_banner("Create OIDC IAM role for Pipelines")
+    try:
+        create_pipeline_oidc_role(cluster_name, cluster_region)
+    except Exception as e:
+        print(e)
+        print("Try running cleanup_pipeline_irsa.py")
+
+    pipeline_oidc_role_name = f"{common.PIPELINE_OIDC_ROLE_NAME_PREFIX}-{cluster_name}"
+    oidc_role_arn = get_role_arn(pipeline_oidc_role_name, cluster_region)
+
+    print_banner("Writing params.env for Pipelines")
+    output_params_file_path = common.OUTPUT_FILE_PATH
+    write_params(
+        oidc_role_arn, cluster_region, output_params_file_path, config_file_path
+    )
+
+    print_banner("SUCCESS")
