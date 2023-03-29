7c8b247431ca9a2e3f94145a26d36d12beb8e0e6
Wenjin Situ <jsitu@amazon.com>
2023-03-29 16:40:42 -0700

    keep only total test, failure, success metric to push to cloud watch

--- tests/canary/scripts/push_stats_to_cloudwatch.py
+++ tests/canary/scripts/push_stats_to_cloudwatch.py
@@ -6,104 +6,88 @@ import os
 
 xml_path = "../canary/integration_tests.xml"
 
+
 def readXML_and_publish_metrics_to_cw():
     if os.path.isfile(xml_path):
         tree = ET.parse(xml_path)
-        testsuite = tree.find('testsuite')
-        failures = testsuite.attrib['failures']
-        errors = testsuite.attrib['errors']
-        tests = testsuite.attrib['tests']
-        success_rate = 1 - (int(failures)/int(tests))
+        testsuite = tree.find("testsuite")
+        failures = testsuite.attrib["failures"]
+        tests = testsuite.attrib["tests"]
+        successes = tests - failures
     else:
         failures = 0
-        errors = 0
-        tests = 0
-        success_rate = 0
-    
+        successes = 0
+        tests = 1
+
     timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
+    print(f"Failures: {failures}")
+    print(f"Total tests: {tests}")
 
-    print (f"Failures: {failures}")
-    print (f"Errors: {errors}")
-    print (f"Total tests: {tests}")
-    print (f"Success_rate: {success_rate}")
-
-    #push to cloudwatch
+    # push to cloudwatch
     cw_client = boto3.client("cloudwatch")
     codebuild_client = boto3.client("codebuild")
 
-    codebuild_projects = codebuild_client.list_projects()['projects']
+    codebuild_projects = codebuild_client.list_projects()["projects"]
     print(codebuild_projects)
     project_name = codebuild_projects[0]
 
     # Define the metric data
     metric_data = [
         {
-            'MetricName': 'failures',
-            'Timestamp': timestamp,
-            'Dimensions': [
-                {'Name': "CodeBuild Project Name", "Value": project_name},
+            "MetricName": "failures",
+            "Timestamp": timestamp,
+            "Dimensions": [
+                {"Name": "CodeBuild Project Name", "Value": project_name},
             ],
-            'Value': int(failures),
-            'Unit': 'Count'
+            "Value": int(failures),
+            "Unit": "Count",
         },
         {
-            'MetricName': 'errors',
-            'Timestamp': timestamp,
-            'Dimensions': [
-                {'Name': "CodeBuild Project Name", "Value": project_name},
+            "MetricName": "total_tests",
+            "Timestamp": timestamp,
+            "Dimensions": [
+                {"Name": "CodeBuild Project Name", "Value": project_name},
             ],
-            'Value': int(errors),
-            'Unit': 'Count'
+            "Value": int(tests),
+            "Unit": "Count",
         },
         {
-            'MetricName': 'total_tests',
-            'Timestamp': timestamp,
-            'Dimensions': [
-                {'Name': "CodeBuild Project Name", "Value": project_name},
+            "MetricName": "successes",
+            "Timestamp": timestamp,
+            "Dimensions": [
+                {"Name": "CodeBuild Project Name", "Value": project_name},
             ],
-            'Value': int(tests),
-            'Unit': 'Count'
+            "Value": successes,
+            "Unit": "Count",
         },
-        {
-            'MetricName': 'successes_rate',
-            'Timestamp': timestamp,
-            'Dimensions': [
-                {'Name': "CodeBuild Project Name", "Value": project_name},
-            ],
-            'Value': success_rate,
-            'Unit': 'Percent'
-        }
     ]
 
-# Use the put_metric_data method to push the metric data to CloudWatch
+    # Use the put_metric_data method to push the metric data to CloudWatch
     try:
         response = cw_client.put_metric_data(
-            Namespace='Canary_Telemetry',
-            MetricData=metric_data
+            Namespace="Canary_Telemetry", MetricData=metric_data
         )
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
             print("Successfully pushed data to CloudWatch")
             # return 200 status code if successful
             return 200
         else:
             # raise exception if the status code is not 200
-            raise Exception("Unexpected response status code: {}".format(response['ResponseMetadata']['HTTPStatusCode']))
+            raise Exception(
+                "Unexpected response status code: {}".format(
+                    response["ResponseMetadata"]["HTTPStatusCode"]
+                )
+            )
     except Exception as e:
         print("Error pushing data to CloudWatch: {}".format(e))
         # raise exception if there was an error pushing data to CloudWatch
         raise
 
-    
 
-
-
-    
-
-def main(): 
+def main():
     readXML_and_publish_metrics_to_cw()
+
 
 if __name__ == "__main__":
     main()
-
-
