c2212526ff30c04da87c7cd86389a454359dc7bd
ryansteakley <37981995+ryansteakley@users.noreply.github.com>
2023-04-06 10:27:41 -0700

    cherry-pick update to v1.3.2 secrets sync (#651)

--- tests/e2e/resources/external/secrets_store_csi_driver.py
+++ tests/e2e/resources/external/secrets_store_csi_driver.py
@@ -1,16 +1,16 @@
 from e2e.utils.utils import kubectl_apply
 
-RBAC_SECRETS_PROVIDER_CLASS = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.0.0/deploy/rbac-secretproviderclass.yaml"
+RBAC_SECRETS_PROVIDER_CLASS = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.3.2/deploy/rbac-secretproviderclass.yaml"
 
-CSI_DRIVER_V_1_0_0 = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.0.0/deploy/csidriver.yaml"
+CSI_DRIVER_V_1_0_0 = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.3.2/deploy/csidriver.yaml"
 
-SECRETS_STORE_SECRETS_PROVIDER_CLASSES = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.0.0/deploy/secrets-store.csi.x-k8s.io_secretproviderclasses.yaml"
+SECRETS_STORE_SECRETS_PROVIDER_CLASSES = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.3.2/deploy/secrets-store.csi.x-k8s.io_secretproviderclasses.yaml"
 
-SECRETS_STORE_SECRETS_PROVIDER_CLASS_POD_STATUSES = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.0.0/deploy/secrets-store.csi.x-k8s.io_secretproviderclasspodstatuses.yaml"
+SECRETS_STORE_SECRETS_PROVIDER_CLASS_POD_STATUSES = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.3.2/deploy/secrets-store.csi.x-k8s.io_secretproviderclasspodstatuses.yaml"
 
-SECRETS_STORE_CSI_DRIVER = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.0.0/deploy/secrets-store-csi-driver.yaml"
+SECRETS_STORE_CSI_DRIVER = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.3.2/deploy/secrets-store-csi-driver.yaml"
 
-RBAC_SECRETS_PROVIDER_SYNCING = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.0.0/deploy/rbac-secretprovidersyncing.yaml"
+RBAC_SECRETS_PROVIDER_SYNCING = "https://raw.githubusercontent.com/kubernetes-sigs/secrets-store-csi-driver/v1.3.2/deploy/rbac-secretprovidersyncing.yaml"
 
 
 def install():
