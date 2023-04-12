63b749f53ae019820ba34aa7fcbcc5bc3aa7c24e
Steakley <ryanstea@amazon.com>
2023-04-11 23:10:10 -0700

    update aws-authservice

--- components/aws-authservice/main.go
+++ components/aws-authservice/main.go
@@ -14,6 +14,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -36,7 +37,6 @@ func init() {
 // LogoutHandler expires ALB Cookies and redirects to Cognito Logout Endpoint
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Traffic reached LogoutHandler")
-
 	// There are 4 possible AWSELBAuthSessionCookies
 	// https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html#authentication-logout
 	for cookieIndex := 0; cookieIndex < 4; cookieIndex++ {
@@ -44,15 +44,29 @@ func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 		expireALBCookie := &http.Cookie{Value: "Expired", Name: name, MaxAge: -1, Path: "/"}
 		http.SetCookie(w, expireALBCookie)
 	}
-	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+
+	// Central Dashboard expects to redirect to event.detail.response['afterLogoutURL']) after logout
+	// https://github.com/kubeflow/kubeflow/blob/master/components/centraldashboard/public/components/logout-button.js#L49
+	resp := struct {
+		AfterLogoutURL string `json:"afterLogoutURL"`
+	}{
+		AfterLogoutURL: redirectURL,
+	}
+	jsonBytes, err := json.Marshal(resp)
+	if err != nil {
+		log.Println("Failed to marshal struct to json: %v", err)
+	}
+
+	w.Write(jsonBytes)
+
+	http.Redirect(w, r, redirectURL, http.StatusCreated)
 }
 
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/logout", LogoutHandler).Methods(http.MethodGet)
-
+	router.HandleFunc("/authservice/logout", LogoutHandler).Methods(http.MethodPost)
 	var listenPort = ":" + port
 	log.Println("Starting web server at", listenPort)
-	log.Fatal(http.ListenAndServe(listenPort, handlers.CORS()(router)))
+	log.Println(http.ListenAndServe(listenPort, handlers.CORS()(router)))
 }
