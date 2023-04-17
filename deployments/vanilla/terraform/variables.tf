34a28de480b724de882b194de1a6e58bf6103dde
ryansteakley <37981995+ryansteakley@users.noreply.github.com>
2023-04-17 16:42:54 -0700

    update to latest eksctl version and update eks to 1.25 (#682)
    **Which issue is resolved by this Pull Request:**
    Resolves #
    
    **Description of your changes:**
    updates eksctl to support v1.25 eks and changes docs to 1.25 as-well
    
    **Testing:**
    - [ ] Unit tests pass
    - [ ] e2e tests pass
    - Details about new tests (If this PR adds a new feature)
    - Details about any manual tests performed
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.

--- deployments/vanilla/terraform/variables.tf
+++ deployments/vanilla/terraform/variables.tf
@@ -12,7 +12,7 @@ variable "cluster_region" {
 variable "eks_version" {
   description = "The EKS version to use"
   type        = string
-  default     = "1.24"
+  default     = "1.25"
 }
 
 variable "node_instance_type" {
