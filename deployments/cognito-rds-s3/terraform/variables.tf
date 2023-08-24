99b31a2c9bdc15211a26453ffd4f09afe126c25d
ananth102 <abashyam@amazon.com>
2023-08-24 18:47:12 +0000

    adding a variable for disk size

--- deployments/cognito-rds-s3/terraform/variables.tf
+++ deployments/cognito-rds-s3/terraform/variables.tf
@@ -32,6 +32,18 @@ variable "node_instance_type_gpu" {
   default     = null
 }
 
+variable "node_disk_size_cpu" {
+  description = "The disk size of a cpu node."
+  type        = string
+  default     = 50
+}
+
+variable "node_disk_size_gpu" {
+  description = "The disk size of a gpu node."
+  type        = string
+  default     = 75
+}
+
 variable "kf_helm_repo_path" {
   description = "Full path to the location of the helm repo for KF"
   type        = string
