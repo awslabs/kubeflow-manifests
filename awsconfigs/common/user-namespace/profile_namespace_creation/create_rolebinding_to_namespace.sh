ae9e7b67f9381328926d0a03575eab8672077d85
Justin <justin.howard@ardentmc.com>
2024-10-23 09:59:14 -0500

    added shell script to create new rolebinding, untested.

---
+++ awsconfigs/common/user-namespace/profile_namespace_creation/create_rolebinding_to_namespace.sh
@@ -0,0 +1,21 @@
+export PROFILE_NAMESPACE=test-user
+export PROFILE_USER=test.user@ardentmc.com
+
+cat <<EOF > rolebinding-namespace-account-editor.yaml
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: default-editor
+  namespace: ${PROFILE_NAMESPACE}
+  annotations:
+    role: edit
+    user: ${PROFILE_USER}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: kubeflow-edit
+subjects:
+- kind: ServiceAccount
+  name: default-editor
+  namespace: ${PROFILE_NAMESPACE}
+EOF
